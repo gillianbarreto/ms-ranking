d1820092e1da9077ca70713efc1b693f6fd05027
Gillian Barreto Arias <gillianbarretoa@gmail.com>
2021-03-08 10:20:16 -0300

    feat: eventEmitter in getRankingBoard

--- src/app/controllers/v1/ranking.controller.js
+++ src/app/controllers/v1/ranking.controller.js
@@ -1,3 +1,5 @@
+const EventEmitter = require('events');
+
 import { rankingModule, rankingBoardModule } from '../../modules/v1/ranking.module';
 
 export const rankingController = async (req, res) => {
@@ -9,4 +11,19 @@ export const rankingController = async (req, res) => {
 export const rankingBoardController = async (req, res) => {
   const response = await rankingBoardModule();
   return res.status(response.code).send(response);
+};
+
+export const rankingBoardEventController = async (req, res) => {
+  const stream = new EventEmitter();
+
+  res.set('Content-Type', 'text/event-stream;charset=utf-8');
+  res.set('Cache-Control', 'no-cache');
+  stream.on('push', (event, data) => {
+    res.write(`event: ${String(event)}\n data: ${JSON.stringify(data)}\n\n`)
+  });
+
+  setInterval(async () => {
+    const response = await rankingBoardModule();
+    stream.emit('push', 'message', response)
+  }, 10000);
 };
--- src/routes/v1/rankingRoutes.js
+++ src/routes/v1/rankingRoutes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { validateRankingRequest, validateScoreRequest, validate } from '../../app/middlewares/request.validator';
-import { rankingController, rankingBoardController } from '../../app/controllers/v1/ranking.controller';
-import { scoreController } from '../../app/controllers/v1/score.controller'
+import { rankingController, rankingBoardController, rankingBoardEventController } from '../../app/controllers/v1/ranking.controller';
+import { scoreController } from '../../app/controllers/v1/score.controller';
 
 const RankingRoutes = Router();
 
 RankingRoutes.get('/ranking-board', rankingBoardController);
+RankingRoutes.get('/ranking-board-event', rankingBoardEventController);
 RankingRoutes.post('/ranking', validateRankingRequest(), validate, rankingController);
 RankingRoutes.post('/score', validateScoreRequest(), validate, scoreController);
 
